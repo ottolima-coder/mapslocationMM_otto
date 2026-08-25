6467e1e86e4592d50d5dba5f325e2658e3481066
ottolima-coder <otto.lima@shippify.co>
2026-08-25 12:41:48 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDSdtJJJaarmHRGE0SSf8MmSEdlwg_umFE",
+  authDomain: "monitoramentomm-1fb7a.firebaseapp.com",
+  databaseURL: "https://monitoramentomm-1fb7a-default-rtdb.firebaseio.com",
+  projectId: "monitoramentomm-1fb7a",
+  storageBucket: "monitoramentomm-1fb7a.firebasestorage.app",
+  messagingSenderId: "784467433289",
+  appId: "1:784467433289:web:6cf7d8e45f167e49ef6c98",
+  measurementId: "G-N1NF0PMJKG"
+};
